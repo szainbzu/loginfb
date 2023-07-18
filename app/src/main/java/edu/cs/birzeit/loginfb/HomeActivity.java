19a1959e8ff82c12523e31ba41411696ea47cbd4
szainbzu <szain@birzeit.edu>
2023-07-18 10:10:21 +0200

    version1

--- app/src/main/java/edu/cs/birzeit/loginfb/HomeActivity.java
+++ app/src/main/java/edu/cs/birzeit/loginfb/HomeActivity.java
@@ -1,9 +1,12 @@
 package edu.cs.birzeit.loginfb;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +17,26 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    private TextView txtUser;
+    private TextView txtUser, txtQuote;
     private EditText edtMovie, edtQuote;
-    private Button btnSave;
+    private Button btnSave, btnShow;
+    public static final String QUOTE = "QUOTE1";
+    public static final String MOVIE = "MOVIE1";
     private FirebaseFirestore database = FirebaseFirestore.getInstance();
+    private DocumentReference documentRef = database.collection("movie_quotes")
+            .document("quotes");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +45,9 @@ public class HomeActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btnSave);
         edtMovie = findViewById(R.id.edtMovie);
         edtQuote = findViewById(R.id.edtQuote);
+
+        txtQuote = findViewById(R.id.txtQuote);
+        btnShow  = findViewById(R.id.btnShow);
 
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -47,10 +62,9 @@ public class HomeActivity extends AppCompatActivity {
                 Map<String, String> map = new HashMap<>();
                 String movie = edtMovie.getText().toString();
                 String quote = edtQuote.getText().toString();
-                map.put("MOVIE1", movie);
-                map.put("QUOTE1", quote);
-                database.collection("movie_quotes")
-                        .document("quotes")
+                map.put(MOVIE, movie);
+                map.put(QUOTE, quote);
+                documentRef
                         .set(map)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
@@ -66,10 +80,59 @@ public class HomeActivity extends AppCompatActivity {
                                         "Data save failed", Toast.LENGTH_SHORT).show();
                             }
                         });
+
+                btnShow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        documentRef.get()
+                                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        if(documentSnapshot.exists()){
+                                            String movie = documentSnapshot.getString(MOVIE);
+                                            String quote = documentSnapshot.getString(QUOTE);
+
+                                            txtQuote.setText(movie + "\n" + quote);
+                                        }
+
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(HomeActivity.this, "could not get data", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                });
             }
         });
 
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        documentRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+               if(error != null){
+                   Toast.makeText(HomeActivity.this, "Something went wrong",
+                           Toast.LENGTH_SHORT).show();
+                   Log.e("ERROR", error.toString());
+                   return;
+               }
+               if(value!=null && value.exists() ){
+                   String movie = value.getString(MOVIE);
+                   String quote = value.getString(QUOTE);
+
+                   txtQuote.setText(movie + "\n" + quote);
+
+               }
+
+            }
+        });
     }
 }
